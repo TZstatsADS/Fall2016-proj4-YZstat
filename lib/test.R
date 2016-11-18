684effeb95a08ee4846f74ad35d5288b5b649ce3
YZstat <yz2896@columbia.edu>
2016-11-18 18:34:27 -0500

    Update test.R

--- lib/test.R
+++ lib/test.R
@@ -1,11 +1,11 @@
-
+library(caret)
 
 load("~/Desktop/Project4_data/fit.RData") ### fit 
 load("~/Desktop/Project4_data/feature_music.RData") ### feature.music==train.feature
 
 Y<-c()
 for (i in 1:2350){
-  Y[i]<-which(fit1$document_sums[,i]==max(fit1$document_sums[,i]))
+  Y[i]<-which(fit$document_sums[,i]==max(fit$document_sums[,i]))
 }
 
 
@@ -16,18 +16,27 @@ test.music<-function(train.feature,
   
   hahaha<-knn3Train(train.feature[,-14], test.feature[,-14],
                     train.topic, k = 5, prob = TRUE) 
-  heihei<-attributes(hahaha)$prob
+  
+  
+  a<-attributes(hahaha)$prob[,1:10]#1-10
+  b<-attributes(hahaha)$prob[,11:17]#12-18
+  c<-attributes(hahaha)$prob[,18]#20
+  d<-rep(0,100)
+  
+  heihei<-cbind(a,d,b,d,c)
+  
+  finally<-heihei %*% model.fit$topics
+  
   
   n<-nrow(test.feature)
   
-  finally<-attributes(hahaha)$prob %*% model.fit$topics
-  
   for (i in 1:n){
-    finally[i,]<-order(finally[i,])
+    finally[i,]<-order(finally[i,],decreasing = TRUE)
   }
   
   return(finally)
   
 }
 
+test_label<-test.music(feature.music,test_feature, Y,fit)
 
