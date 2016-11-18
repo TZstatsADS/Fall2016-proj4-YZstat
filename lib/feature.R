5e1c56ee1e3f18f77ea6a90e25ad61ba307f0a38
YZstat <yz2896@columbia.edu>
2016-11-18 18:30:25 -0500

    Update feature.R

--- lib/feature.R
+++ lib/feature.R
@@ -6,26 +6,37 @@ library(LDAvis)
 
 
 ##### Read Data
-
+# 
 # mydata<-list.files(path="~/Desktop/Project4_data/data", recursive = TRUE)
 # setwd("~/Desktop/Project4_data/data")
 # 
+# 
 # H5close()
 # 
-# X_train<-list()
-# 
 # for (i in 1:length(mydata)){
-#   
+# 
 #   X_train[[i]]<-h5read(mydata[i],"/analysis")
 # }
+
+
+mydata<-list.files(path="~/Desktop/TestSongFile100", recursive = TRUE)
+H5close()
+setwd("~/Desktop/TestSongFile100")
+
+X_test<-list()
+for (i in 1:length(mydata)){
+
+  X_test[[i]]<-h5read(mydata[i],"/analysis")
+}
+
 
 
 create_variable<-function(name,dataset,rank){
   
   name<-dataset[[1]][rank]
   nlength<-length(unlist(dataset[[1]][rank]))
-  
-  for (i in 2:2350){
+ 
+  for (i in 2:100){
     name<-rbind(name,dataset[[i]][rank])
     nlength<-rbind(nlength,length(unlist(dataset[[i]][rank])))
   }
@@ -35,9 +46,9 @@ create_variable<-function(name,dataset,rank){
   colnames(final_matrix)[2]<-c("length")
   
   n<-round(mean(unlist(final_matrix[,2])))
-  K<-matrix(data=0, nrow = 2350, ncol = n)
+  K<-matrix(data=0, nrow = 100, ncol = n)
   
-  for (i in 1:2350){
+  for (i in 1:100){
     if (final_matrix[,2][[i]] < n){
       haha<-rep(final_matrix[[i]],n)
       K[i,]<-haha[1:n]
@@ -61,8 +72,8 @@ variable.feature<-function(variable,num.center){
 
 create.feature<-function(dataset,num.center){
   
-  n<-ncol(dataset)
-  m<-nrow(dataset)
+  n<-15
+  m<-100
   
   X_bars_confidence <- create_variable(bars_confidence,dataset,1)
   X_bars_start <- create_variable(bars_start,dataset,2)
@@ -77,11 +88,11 @@ create.feature<-function(dataset,num.center){
   X_segments_pitches <- create_variable(segments_pitches,dataset,11)
   X_segments_start <- create_variable(segments_start,dataset,12)
   X_segments_timbre <- create_variable(segments_timbre,dataset,13)
-  X_songs <- create_variable(songs,dataset,14)
-  X_tatums_confidence <- create_variable(tatums_confidence,dataset,15)
-  X_tatums_start <- create_variable(tatums_start,dataset,16)
+  #X_songs <- create_variable(songs,dataset,14)
+  X_tatums_confidence <- create_variable(tatums_confidence,dataset,14)
+  X_tatums_start <- create_variable(tatums_start,dataset,15)
   
-  feature.music<-matrix(NA, nrow = m, ncol = n)
+  feature.music<-matrix(NA,nrow = 100, ncol = 15)
   
   feature.music[,1]<-variable.feature(X_bars_confidence,num.center)
   feature.music[,2]<-variable.feature(X_bars_start,num.center)
@@ -96,10 +107,16 @@ create.feature<-function(dataset,num.center){
   feature.music[,11]<-variable.feature(X_segments_pitches,num.center)
   feature.music[,12]<-variable.feature(X_segments_start,num.center)
   feature.music[,13]<-variable.feature(X_segments_timbre,num.center)
-  feature.music[,14]<-variable.feature(X_song,num.center)
-  feature.music[,15]<-variable.feature(X_tatums_confidence,num.center)
-  feature.music[,16]<-variable.feature(X_tatums_start,num.center)
+  #feature.music[,14]<-variable.feature(X_song,num.center)
+  feature.music[,14]<-variable.feature(X_tatums_confidence,num.center)
+  feature.music[,15]<-variable.feature(X_tatums_start,num.center)
   
+  return(feature.music)
   
 }
 
+
+test_feature<-create.feature(dataset=X_test, num.center=20)
+
+
+  
